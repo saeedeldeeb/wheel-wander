51cc7ecf7477ca3984b5cd9b75837870ede5b77e
Saeed Eldeeb <saeedeldeeb1@gmail.com>
2023-07-15 01:33:35 +0300

    Added AdminLte template

---
+++ src/main/java/com/eldeeb/wheel/wheelwander/controllers/AuthController.java
@@ -0,0 +1,24 @@
+package com.eldeeb.wheel.wheelwander.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/forgot-password")
+    public String forgotPassword() {
+        return "forgot-password";
+    }
+
+    @GetMapping("/reset-password")
+    public String resetPassword() {
+        return "reset-password";
+    }
+}
