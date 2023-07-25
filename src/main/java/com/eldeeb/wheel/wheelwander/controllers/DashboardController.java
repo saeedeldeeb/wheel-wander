9f20913b69494fed0f0a168f95d8fc57647b4ea1
Saeed Eldeeb <saeedeldeeb1@gmail.com>
2023-07-26 02:02:04 +0300

    Architected my admin view

---
+++ src/main/java/com/eldeeb/wheel/wheelwander/controllers/DashboardController.java
@@ -0,0 +1,14 @@
+package com.eldeeb.wheel.wheelwander.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+    @GetMapping("/home")
+    public String home() {
+        return "layouts/layout";
+    }
+}
